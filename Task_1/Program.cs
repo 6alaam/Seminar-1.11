9838d498d0b03796bce418a8035e9b17dce70741
Balaam <ballaam@yandex.ru>
2023-11-01 12:06:50 +0300

    ain

---
+++ Task_1/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3 , 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+using System.Globalization;
+using System.Security.Cryptography;
+
+int a = ReadInt("a");
+int b = ReadInt("b");
+int c = Degree(a, b);
+System.Console.WriteLine(c);
+
+
+
+int ReadInt(string argument)    // запрос на ввод с клавиатуры и проверка на точ то введенн тип Int если нет выдаст сообщение
+{
+
+    Console.Write($"input {argument} : ");
+    int i;
+    while (!int.TryParse(Console.ReadLine(), out i))
+    {
+        System.Console.WriteLine("It's not a number");
+
+    }
+    return i;
+}
+
+int Degree(int number, int deg)
+{
+
+    int stepen = 1;
+    for (int i = 1; i <= deg; i++)
+    {
+        stepen = stepen * number;
+    }
+    return stepen;
+
+}
---
+++ Task_3/Program.cs
@@ -0,0 +1,37 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] array = new int[8]; //  создание массива с 8 элементами заполнен нулями
+
+
+void FillArray(int[] collection)  // метод заполнения массива рандомными числами от 0 до 1
+{
+
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next();
+        index++;
+
+    }
+
+}
+
+void PrintArray(int[] col)      // распечатываем все элементы массива
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write(col[position]);
+        System.Console.Write(" ");
+        position++;
+    }
+
+}
+FillArray(array);
+PrintArray(array);
